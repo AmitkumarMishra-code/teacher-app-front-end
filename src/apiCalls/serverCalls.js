dec4a2f083866bb152e3e8a0f5b771ff921e044f
Amitkumar Mishra <mr.mash@gmail.com>
2021-09-17 14:31:16 +0530

    changes

--- src/apiCalls/serverCalls.js
+++ src/apiCalls/serverCalls.js
@@ -1,6 +1,6 @@
 const URL = `https://email-marketing-app.herokuapp.com`
 export async function registerUser(username, name, subject) {
-    console.log(username, name)
+    console.log(username, name, subject)
     try {
         let response = await fetch(URL + '/user', {
             method: 'POST',
@@ -27,20 +27,6 @@ export async function registerUser(username, name, subject) {
 export async function getAffiliateData() {
     try {
         let response = await fetch(URL + '/admin')
-        let data = await response.json()
-        if (response.status !== 200) {
-            throw new Error(data.message)
-        } else {
-            return data.message
-        }
-    } catch (error) {
-        throw error
-    }
-}
-
-export async function registerClick(id) {
-    try {
-        let response = await fetch(URL + '/' + id)
         let data = await response.json()
         if (response.status !== 200) {
             throw new Error(data.message)
